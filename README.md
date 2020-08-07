0328433966aa65dd54f383e06fa536b66fb4ab8e
Chris Love <christopherlove68@gmail.com>
2020-08-07 16:02:36 -0700

    Updates

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Support custom Fedora-based WSL2 distributions.  The distributions are built based on custom Fedora 32 Docker images from which the filesystem is exported as a tarball.
 
 ```bash
-# ./build.sh [base|podman]
+# ./build.sh [--user=<user>][--distro=<distroType>]
 ```
 
 The tarball then can be imported as a WSL distribution from Powershell:
@@ -22,3 +22,11 @@ This is a small image based on the Fedora 32 Docker base image
 This image is based on the Fedora 32 Docker base image with packages added to support Podman and Buildah.
 
 - NOTE: this is a work in progress in trying to get rootless Podman working under WSL2.
+
+## Alternative Docker support
+The alternative way to achieve container support within a WSL2 distro is as follows:
+1. Install [Docker Desktop for Windows](https://hub.docker.com/editions/community/docker-ce-desktop-windows), and enable the WSL2 backend.
+2. Import the Fedora WSL2 distro as shown above.
+3. From the Docker Desktop select Settings -> General -> WSL Integration and choose the Fedora WSL2 distro which was imported.  This will create the `/usr/bin/docker` symlink and add the `docker` group in `/etc/group`.
+4. Start the Fedora WSL2 distro.
+5. As root or via `sudo` add the non-root user to the `docker` group in `/etc/group`.
--- build.sh
+++ build.sh
@@ -1,34 +1,92 @@
 #!/bin/bash
 
-image=$1
+function buildDistro()
+{
+    local dir=$1
+    local user=$2
+    local distro=$3
+
+    # Build image from Dockerfile
+    pushd $dir
+    cp Dockerfile.template Dockerfile
+    sed -i "s/USER/$user/g" Dockerfile
+    podman build -t fedora32-wsl2-$distro:latest .
+    ret=$?
+    [ $ret -ne 0 ] && { echo "$image image build failed"; exit 1; }
+    popd
+    # Create container to grab filesystem snapshot
+    podman run --name fedora-ctr fedora32-wsl2-$distro cat /etc/fedora-release
+    [ $? -ne 0 ] && { echo "Error creating container using $image image"; exit 1; }
+    # Grab filesystem snapshot as a tarball
+    podman export fedora-ctr > wsl-fedora32-$distro.tar
+    [ $? -ne 0 ] && { echo "Error exporting filesystem tarball from $image container"; exit 1; }
+    # Cleanup the container
+    podman rm fedora-ctr
+
+}
+
+function showUsage()
+{
+    cat << EOT
+Usage:
+    $(basename $0) [--user <user>] [--distro <distroType>]
+
+--user=<user>           - specify the non-root username which will be created with sudo access
+--distro=<distroType>   - specify the type of distro to build:
+                            - base   - minimal Fedora distro
+                            - podman - Fedora distro with Podman, Buildah (NOTE: work in progress)
+EOT
+return 1
+}
+
+# Parameter defaults
+image=base
+user=user
+
+# Handle command-line arguments
+while test $# -gt 0; do
+    param="$1"
+    if test "${1::1}" = "-"; then
+        if test ${#1} -gt 2 -a "${1::2}" = "--" ; then
+            param="${1:2}"
+        else
+            param="${1:1}"
+        fi
+    else
+        break
+    fi
+
+    shift
+
+    case $param in
+        user=*)
+            user=$(echo $param|cut -f2 -d'=')
+            ;;
+        distro=*)
+            distro=$(echo $param|cut -f2 -d'=')
+            ;;
+        help|h|?|-?)
+            showUsage
+            exit 0
+            ;;
+        *)
+            echo "Error: Unknown parameter: $param"
+            showUsage
+            exit 2
+            ;;
+    esac
+
+done
 
 case $image in
 base)
-    # Build image from Dockerfile
-    pushd base
-    podman build -t fedora32-wsl2-base:latest .
-    ret=$?
-    [ $? -ne 0 ] && { echo "$image image build failed"; exit 1; }
-    popd
-    # Create container to grab filesystem snapshot
-    podman run --name fedora-ctr fedora32-wsl2-base cat /etc/fedora-release
-    # Grab filesystem snapshot as a tarball
-    podman export fedora-ctr > wsl-fedora32-base.tar
-    # Cleanup the container
-    podman rm fedora-ctr
+    buildDistro base "$user" base
     ;;
 podman)
-    # Build image from Dockerfile
-    pushd podman
-    podman build -t fedora32-wsl2-podman:latest .
-    ret=$?
-    [ $? -ne 0 ] && { echo "$image image build failed"; exit 1; }
-    popd
-    # Create container to grab filesystem snapshot
-    podman run --name fedora-ctr fedora32-wsl2-podman cat /etc/fedora-release
-    # Grab filesystem snapshot as a tarball
-    podman export fedora-ctr > wsl-fedora32-podman.tar
-    # Cleanup the container
-    podman rm fedora-ctr
-    ;;    
+    buildDistro podman "$user" podman
+    ;;
+*)
+    echo "Unsupported distro type $image"
+    exit 1
+    ;;  
 esac
